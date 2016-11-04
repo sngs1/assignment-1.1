4fdc820674914b1ce9b06b50db3610502b3f11e7
GANGA C K <kochupotty@gmail.com>
2016-11-04 14:54:11 +0530

    assignment1.1

---
+++ a11.java
@@ -0,0 +1,20 @@
+package a9;
+
+public class a11 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+     int num1=44;       // initialization of variables
+     int num2=25;
+     int sum=0;
+     while((num2--)!=0)   // using while loop for iteration
+     {
+    	 num1++;          // using increment operator
+     }
+	sum=num1;
+	System.out.println("Sum="+sum);
+	}
+	
+
+}
